ddd83faa36c0440f294cfb2b181108b7052b8126
pratik.datir@siemens.com <pratik.datir@siemens.com>
2022-08-31 12:26:24 +0530

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"os"
+
+	"github.com/go-serverless-test/pkg/handlers"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var (
+	dynamoServer dynamodbiface.DynamoDBAPI
+)
+
+func main() {
+	region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)})
+	if err != nil {
+		return
+	}
+	dynamoServer = dynamodb.New(awsSession)
+	lambda.Start(handler)
+}
+
+const tableName = "go-serverless-test-table"
+
+func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
+	case "GET":
+		return handlers.GetUser(req, tableName, dynamoServer)
+	case "POST":
+		return handlers.CreateUser(req, tableName, dynamoServer)
+	case "PUT":
+		return handlers.UpdateUser(req, tableName, dynamoServer)
+	case "DELETE":
+		return handlers.DeleteUser(req, tableName, dynamoServer)
+	default:
+		return handlers.UnhandledMethodResponse(req, tableName, dynamoServer)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module go-serverless-test
+
+go 1.17
+
+require (
+	github.com/aws/aws-lambda-go v1.34.1
+	github.com/aws/aws-sdk-go v1.44.88
+	github.com/aws/aws-sdk-go-v2 v1.16.12
+	github.com/aws/aws-sdk-go-v2/service/lambda v1.24.2
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.19 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.13 // indirect
+	github.com/aws/smithy-go v1.13.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
